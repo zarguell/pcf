b4e0429fbbaaeef8560128524909f87b6c38eeb7
Automated <actions@users.noreply.github.com>
2024-01-27 04:27:32 +0000

    Sync pcf: Sat Jan 27 04:27:32 UTC 2024

--- fix_scripts/new_notes_type_field/note_type_add.py
+++ fix_scripts/new_notes_type_field/note_type_add.py
@@ -7,9 +7,12 @@ def note_type_add(config, db):
     print("More info: https://gitlab.com/invuls/pentest-projects/pcf/-/issues/156")
     print("Fixing Notes table")
     try:
-        db.execute("ALTER TABLE Notes ADD `type` text default 'html';")
+        db.execute("ALTER TABLE Notes ADD type text default 'html';")
         db.conn.commit()
     except sqlite3.OperationalError as e:
         print("Don't need to add a new column!")
+    except Exception as e:
+        print("Unhandled exception during process of creating a new column - 'type' "
+              "for Notes table. Please, create a bug ticket and attach the exception!")
+        print("Exception:", e)
     print("Fixed Notes table!")
-
